3abd4ff402ccf798496a1e0d8969419f43fde2a5
Liang Yu <yuliang@MIT.edu>
2016-02-08 18:59:19 -0500

    Fixed wget script

--- getfiles.sh
+++ getfiles.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
-cnt=44
-
-for ((cnt=75; cnt<=99; cnt++))
+for ((xx=22; xx<=28; xx++))
 do
-	wget -r -l1 -H -t1 -nd -N -A.gz -erobots=off https://archive.stsci.edu/pub/k2/target_pixel_files/c6/212200000/$cnt\000/
-	gunzip *2122$cnt\*
+	for ((yy=10; yy=99; yy++))
+	do
+		wget -r -l1 -H -nd -A.gz -erobots=off https://archive.stsci.edu/pub/k2/target_pixel_files/c6/21$xx\00000/$yy\000/
+	done
+
+	for ((y=0; y=9; yy++))
+	do
+		wget -r -l1 -H -nd -A.gz -erobots=off https://archive.stsci.edu/pub/k2/target_pixel_files/c6/21$xx\00000/0$y\000/
+	done
+	gunzip ktwo*gz
 done
+
